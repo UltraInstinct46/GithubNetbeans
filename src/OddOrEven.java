72cf8315cfb110e701687009ebf0a9ba42a7c6d8
UltraInstinct46 <restuaerestu291@gmail.com>
2023-01-05 14:19:30 +0700

    Update OddOrEven.java

---
+++ src/OddOrEven.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author killua
+ */
+public class OddOrEven 
+{        
+    
+    public static void CheckOddOrEven() 
+    {
+         System.out.println("Enter an integer to check if even or odd"); 
+       int x;
+      Scanner in = new Scanner(System.in);
+      x = in.nextInt();
+ 
+      if ( x % 2 == 0 )
+         System.out.println("The number entered is an even number.");
+      else
+         System.out.println("The number entered is an odd number."); 
+      
+    }
+
+}
